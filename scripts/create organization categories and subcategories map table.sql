9481fb0339d38beab257a4b0d6bd6c1b531c8c32
Justin <justin.halse@gmail.com>
2023-09-08 12:32:12 +0200

    create mapping table for orgs and categories

---
+++ scripts/create organization categories and subcategories map table.sql
@@ -0,0 +1,28 @@
+-- Table: charity.organizations_categories_map
+-- DROP TABLE IF EXISTS charity.organizations_categories_map;
+
+CREATE TABLE IF NOT EXISTS charity.organizations_categories_map
+(
+    id bigint NOT NULL,
+    organization_id uuid NOT NULL,
+    category_id integer NOT NULL,
+    subcategory_id integer,
+    CONSTRAINT organizations_categories_map_pkey PRIMARY KEY (id),
+    CONSTRAINT organizations_categories_map_categories_fkey FOREIGN KEY (category_id)
+        REFERENCES charity.categories (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT organizations_categories_map_organizations_fkey FOREIGN KEY (organization_id)
+        REFERENCES charity.organizations (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT organizations_categories_map_subcategories_fkey FOREIGN KEY (subcategory_id)
+        REFERENCES charity.subcategories (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS charity.organizations_categories_map
+    OWNER to postgres;
