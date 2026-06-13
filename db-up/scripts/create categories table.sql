95d86931ae52f037a18cb8333a692ef76bc88cdf
Justin <justin.halse@gmail.com>
2026-06-13 15:19:06 +0200

    categories table def for sql server

--- db-up/scripts/create categories table.sql
+++ db-up/scripts/create categories table.sql
@@ -1,17 +1,28 @@
--- Table: charity.categories
--- DROP TABLE IF EXISTS charity.categories;
+-- noinspection SqlNoDataSourceInspectionForFile
 
-CREATE TABLE IF NOT EXISTS charity.categories
+USE [SharingHope]
+GO
+
+/****** Object:  Table [dbo].[Categories_Old]    Script Date: 2026/06/13 14:44:43 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [charity].[tCategories](
+    [CategoryId] [INT] NOT NULL,
+    [Name] [NVARCHAR](255) NOT NULL,
+    [Description] [NVARCHAR](255) NOT NULL,
+    [UiIconName] [VARCHAR](25) NOT NULL,
+    [CreatedOnUtc] DATETIME2 NOT NULL DEFAULT(GETDATE()),
+    [UpdatedOnUtc] DATETIME2 NULL,
+    [IsDeleted] BIT
+
+    CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED
 (
-    category_id integer NOT NULL,
-    name text COLLATE pg_catalog."default",
-    description text COLLATE pg_catalog."default",
-    CONSTRAINT categories_pkey PRIMARY KEY (category_id),
-    mobile_icon_name text COLLATE pg_catalog."default"
+[CategoryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+    ) ON [PRIMARY]
+    GO
 
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS charity.categories
-    OWNER to postgres;
