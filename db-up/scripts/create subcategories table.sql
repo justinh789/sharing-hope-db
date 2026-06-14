3c2c554bccda330f4b37ccf56e1e07976b43e94f
Justin <justin.halse@gmail.com>
2026-06-14 22:06:41 +0200

    Created on, updated on and isdeleted

--- db-up/scripts/create subcategories table.sql
+++ db-up/scripts/create subcategories table.sql
@@ -5,10 +5,13 @@ DROP TABLE charity.tSubcategories;
 -- Create table
 CREATE TABLE charity.tSubcategories
 (
-    SubCategoryId INT NOT NULL PRIMARY KEY,
+    [SubCategoryId] INT NOT NULL PRIMARY KEY,
     [Name] NVARCHAR(255) NOT NULL,
     [Description] NVARCHAR(4000) NOT NULL,
     [CategoryId] INT NOT NULL,
+    [CreatedOnUtc] [datetime2] (7) NOT NULL CONSTRAINT [DF_tSubcategories_CreatedOn] DEFAULT (getdate()),
+    [UpdatedOnUtc] [datetime2] (7) NULL,
+    [IsDeleted] [bit] NULL DEFAULT(0)
     CONSTRAINT FK_tSubcategories_Categories
         FOREIGN KEY (CategoryId) REFERENCES charity.tCategories(CategoryId)
-    );
+);
