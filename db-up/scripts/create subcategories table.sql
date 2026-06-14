8ae630eb1314c4373aecaaf6f9c426b0abcb8c24
Justin <justin.halse@gmail.com>
2026-06-14 21:50:13 +0200

    sub category table def

--- db-up/scripts/create subcategories table.sql
+++ db-up/scripts/create subcategories table.sql
@@ -1,16 +1,14 @@
--- Table: charity.categories
--- DROP TABLE IF EXISTS charity.categories;
+-- Drop table if it exists
+IF OBJECT_ID('charity.tSubcategories', 'U') IS NOT NULL
+DROP TABLE charity.tSubcategories;
 
-CREATE TABLE IF NOT EXISTS charity.subcategories
+-- Create table
+CREATE TABLE charity.tSubcategories
 (
-    subcategory_id integer NOT NULL,
-    name text COLLATE pg_catalog."default",
-    description text COLLATE pg_catalog."default",
-    category_id integer REFERENCES charity.categories (category_id),
-    CONSTRAINT subcategories_pkey PRIMARY KEY (subcategory_id)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS charity.subcategories
-    OWNER to postgres;
+    SubCategoryId INT NOT NULL PRIMARY KEY,
+    [Name] NVARCHAR(255) NOT NULL,
+    [Description] NVARCHAR(4000) NOT NULL,
+    [CategoryId] INT NOT NULL,
+    CONSTRAINT FK_tSubcategories_Categories
+        FOREIGN KEY (CategoryId) REFERENCES charity.tCategories(CategoryId)
+    );
