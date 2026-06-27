1687551fed90dab87421d4c1b10620c3dd7aad57
Justin <justin.halse@gmail.com>
2026-06-27 15:08:24 +0200

    org category and subcategory map

--- db-up/scripts/create organization categories and subcategories map table.sql
+++ db-up/scripts/create organization categories and subcategories map table.sql
@@ -1,28 +1,28 @@
--- Table: charity.organizations_categories_map
--- DROP TABLE IF EXISTS charity.organizations_categories_map;
+USE SharingHope
 
-CREATE TABLE IF NOT EXISTS charity.organizations_categories_map
+IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'charity')
+    EXEC('CREATE SCHEMA charity');
+GO
+
+
+
+CREATE TABLE charity.tOrganizationsCategoriesMap
 (
-    organizations_categories_map_id bigint NOT NULL,
-    organization_id uuid NOT NULL,
-    category_id integer NOT NULL,
-    subcategory_id integer,
-    CONSTRAINT organizations_categories_map_pkey PRIMARY KEY (organizations_categories_map_id),
-    CONSTRAINT organizations_categories_map_categories_fkey FOREIGN KEY (category_id)
-        REFERENCES charity.categories (category_id) MATCH SIMPLE
+    OrganizationsCategoriesMapId BIGINT NOT NULL,
+    OrganizationId INT NOT NULL,
+    CategoryId INT NOT NULL,
+    SubCategoryId INT NULL,
+    CONSTRAINT PK_tOrganizationsCategoriesMap PRIMARY KEY (OrganizationsCategoriesMapId),
+    CONSTRAINT FK_tOrganizationsCategoriesMap_tCategories FOREIGN KEY (CategoryId)
+        REFERENCES charity.tCategories(CategoryId)
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT organizations_categories_map_organizations_fkey FOREIGN KEY (organization_id)
-        REFERENCES charity.organizations (organization_id) MATCH SIMPLE
+    CONSTRAINT FK_tOrganizationsCategoriesMap_tOrginizations FOREIGN KEY (OrganizationId)
+        REFERENCES charity.tOrganizations (OrganizationId)
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT organizations_categories_map_subcategories_fkey FOREIGN KEY (subcategory_id)
-        REFERENCES charity.subcategories (subcategory_id) MATCH SIMPLE
+    CONSTRAINT FK_tOrganizationsCategoriesMap_tSubCategories  FOREIGN KEY (SubCategoryId)
+        REFERENCES charity.tSubcategories (SubcategoryId)
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS charity.organizations_categories_map
-    OWNER to postgres;
